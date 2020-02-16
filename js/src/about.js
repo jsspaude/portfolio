7c3ba14704772b1a6a366c11c58eaf24d3412d51
Jess Spaude <jsspaude@gmail.com>
2020-02-15 23:24:13 -0800

    still fixing

---
+++ js/src/about.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+    var topofDiv        = $('.features-content-container').offset().top;
+    var height          = $('.features-content-container').outerHeight();
+    var abtStringType   = $('.special-heading');
+        
+    arrayAbt(abtStringType);
+    
+    $(window).scroll(function(){    
+        if ($(this).scrollTop() > (topofDiv + height - 190)){ 
+            $('.about-graphic-container').addClass('active');
+            $('.special-heading').addClass('active');
+            specialCharActive();
+        }
+    });
+
+    function specialCharActive() {
+
+
+        $('.specialChar').each(function(i) {
+          var specialChar = $(this);
+          setTimeout(function() {
+            specialChar.addClass('active');
+          }, i*150);
+        });
+            
+    }
+
+    function arrayAbt(string) {
+
+        $(string).each(function() {
+    
+            var abtStr           = $(this).html();
+            var abtStrTrm        = abtStr.trim();
+            var abtStr           = abtStrTrm.split("");
+    
+            var abtContents = "";
+            for (var i = 0, len = abtStr.length; i < len; i++) {
+                abtContents += '<span class="specialChar" data-char="' + i + '">' + abtStr[i] + '</span>';
+            }
+    
+            $(this).html(abtContents);
+        });
+    }
+
+});
+
